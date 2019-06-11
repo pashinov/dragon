2fd161e54beffaa1306506f0171201979971b06e
Alexey Pashinov <apashinov@topcon.com>
2019-06-11 13:12:54 +0300

    added config file (#17)

--- rclient/src/main.rs
+++ rclient/src/main.rs
@@ -3,6 +3,9 @@ mod service;
 mod connection;
 
 fn main() {
-    let srv = service::Service::new();
+    let mut settings = config::Config::default();
+    settings.merge(config::File::with_name("/etc/falcon/rclient.yaml")).unwrap();
+
+    let srv = service::Service::new(&settings);
     srv.start();
 }
