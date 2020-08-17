ded390fd75c265801102018994b5cf4c7cadce8f
Alexey Pashinov <pashinov93@gmail.com>
2020-08-17 19:22:48 +0300

    ZMQ subscriber for receiving control messages from iot side

--- src/iot_service/microsvc_controller.hpp
+++ src/iot_service/microsvc_controller.hpp
@@ -26,7 +26,9 @@ namespace iot_service
         void stop();
 
     private:
-        std::thread thread_;
+        std::atomic<bool> alive_;
+        std::thread pub_thread_;
+        std::thread sub_thread_;
         boost::asio::io_service io_service_;
         std::unique_ptr<task_manager> task_manager_;
         std::unique_ptr<phoenix_connector> phoenix_connector_;
